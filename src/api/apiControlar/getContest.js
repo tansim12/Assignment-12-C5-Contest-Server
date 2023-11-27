5f472882073ebf22bd2b692abf9658a6aa27a175
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2023-11-28 04:29:41 +0600

    all contest api pagination is done

--- src/api/apiControlar/getContest.js
+++ src/api/apiControlar/getContest.js
@@ -1,7 +1,12 @@
 const Contest = require("../../Model/contest")
 
 const getContest= async(req , res)=>{
+    const page = parseInt(req.query.page);
+    const size = parseInt(req.query.size);
+    const skip = (page - 1) * size
     const result = await Contest.find({status:"approved"})
-    res.send(result)
+    const allContestData = await Contest.find().skip(skip).limit(size)
+    const totalContestCount = await Contest.estimatedDocumentCount()
+    res.send({result , totalContestCount , allContestData})
 }
 module.exports=getContest
