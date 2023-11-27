604eb0940565499e522be9ff710d48eca55ced94
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2023-11-28 01:03:49 +0600

    all contest data query by status fild

--- src/api/apiControlar/getContest.js
+++ src/api/apiControlar/getContest.js
@@ -1,7 +1,7 @@
 const Contest = require("../../Model/contest")
 
 const getContest= async(req , res)=>{
-    const result = await Contest.find()
+    const result = await Contest.find({status:"approved"})
     res.send(result)
 }
 module.exports=getContest
